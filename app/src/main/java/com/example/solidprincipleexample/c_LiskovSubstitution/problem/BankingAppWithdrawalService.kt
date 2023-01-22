6c76a65611de827bd0b7105a6cfe1642b936d36c
attriaman2520 <attriaman2520@seasiainfotech.com>
2023-01-23 00:22:53 +0530

    -minor code pushed

--- app/src/main/java/com/example/solidprincipleexample/c_LiskovSubstitution/problem/BankingAppWithdrawalService.kt
+++ app/src/main/java/com/example/solidprincipleexample/c_LiskovSubstitution/problem/BankingAppWithdrawalService.kt
@@ -4,6 +4,10 @@ class BankingAppWithdrawalService constructor(private val account: Account) {
 
     fun withDraw(amount: Int) {
         account.withdraw(amount)
+        //not to do this
+        if(!(account is FDAccount)){
+            account.withdraw(amount)
+        }
     }
 
 }
